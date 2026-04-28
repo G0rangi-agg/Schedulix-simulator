8c525706b30aa0272352d52d168cea709b78e5df
G0rangi-agg <aggarwalgorangi@gmail.com>
2026-04-29 00:31:49 +0530

    C++ scheduler backend

---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const { exec } = require("child_process");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/schedule", (req, res) => {
+  const input = req.body.input;
+
+  const process = exec("./scheduler", (error, stdout, stderr) => {
+    if (error) {
+      return res.json({ error: error.message });
+    }
+    res.json({ output: stdout });
+  });
+
+  process.stdin.write(input);
+  process.stdin.end();
+});
+
+app.get("/", (req, res) => {
+  res.send("C++ Scheduler API is running 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
